19e653d1d63fd524bee83a25dff79c5e3ef2daba
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-03-29 17:11:10 -0600

    Sección de Acordión completa

--- src/components/Acordion.jsx
+++ src/components/Acordion.jsx
@@ -1,12 +1,12 @@
 import arrow from "../assets/img/arrow.svg";
 
-export const Acordion = ({ question, answer}) => {
+export const Acordion = ({question, answer}) => {
     return (
-        <div className="relative w-full overflow-hidden rounded-md">
+        <div className="relative w-full overflow-hidden mb-5">
             {/* Question */}
-            <input type="checkbox" className="peer absolute top-0 inset-x-0 w-full h-12 opacity-0 z-10 cursor-pointer"/>
-            <div className="bg-blue-500 h-12 w-full pl-5 flex items-center">
-                <h1 className="text-lg font-semibold text-white">
+            <input type="checkbox" className="peer absolute top-0 inset-x-0 w-full h-20 md:h-12 opacity-0 z-10 cursor-pointer" />
+            <div className=" h-fit w-full p-5 md:p-3 flex items-center border-b-2 border-gray-200">
+                <h1 className="text-2xl font-semibold ">
                     {question}
                 </h1>
             </div>
@@ -15,7 +15,7 @@ export const Acordion = ({ question, answer}) => {
                 <img src={arrow} alt="Arrow" className="w-6 h-6"/>
             </div>
             {/* Answer */}
-            <div className="bg-gray-100 transition-all duration-500 origin-top max-h-0 peer-checked:max-h-80">
+            <div  className="transition-all duration-500 origin-top max-h-0 peer-checked:max-h-80">
                 <div className="p-4">
                     <p>
                         {answer}
