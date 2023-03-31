4474e5405bf33aff3f68108946551e1d175cb4eb
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-03-31 14:09:24 -0600

    Removed empty div

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,6 @@ export const Navbar = () => {
                         <span className="flex">
                             {name}
                         </span>
-                        <div></div>
                     </div>
                 </a>
             </li>
