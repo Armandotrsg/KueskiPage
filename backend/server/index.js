ce951c6eccaf27593bda678861007c1988b6c454
TitanPro47 <santiveraespinoza@gmail.com>
2023-03-30 21:26:33 -0600

    Added Endpoints

--- backend/server/index.js
+++ backend/server/index.js
@@ -21,19 +21,218 @@ app.use(cors({
   origin: 'http://localhost:5173'
 }));
 
-app.get("/api/users", (req, res) => {
-  res.json({ message: "User 1"});
-});
+// Users --------------------------
 
-/*app.get("/api/posts", (req, res) => {
-  fs.readFile( __dirname + "/" + "posts.json", "utf8", (err, data) => {
+app.get("/api/users", (req, res) => {
+  fs.readFile( __dirname + "/" + "users.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(400).send("Error al leer el archivo JSON");
+    }
     console.log( data );
     res.end( data );
   });
-});*/
+});
+
+app.patch("/api/users", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la petición', req.body);
+  res.end( "Recibido!" )
+});
 
 app.post("/api/users", (req, res) => {
-  const {title, content} = req.body;
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+app.delete("/api/users", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+// Identification --------------------------
+
+app.get("/api/identification", (req, res) => {
+  fs.readFile( __dirname + "/" + "users.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(400).send("Error al leer el archivo JSON");
+    }
+    console.log( data );
+    res.end( data );
+  });
+});
+
+app.patch("/api/identification", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la petición', req.body);
+  res.end( "Recibido!" )
+});
+
+app.post("/api/identification", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+app.delete("/api/identification", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+// Identification types --------------------------
+
+app.get("/api/identification_type", (req, res) => {
+  fs.readFile( __dirname + "/" + "users.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(400).send("Error al leer el archivo JSON");
+    }
+    console.log( data );
+    res.end( data );
+  });
+});
+
+app.patch("/api/identification_type", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la petición', req.body);
+  res.end( "Recibido!" )
+});
+
+app.post("/api/identification_type", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+app.delete("/api/identification_type", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+// ARCO Registers--------------------------
+
+app.get("/api/arco_registers", (req, res) => {
+  fs.readFile( __dirname + "/" + "users.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(400).send("Error al leer el archivo JSON");
+    }
+    console.log( data );
+    res.end( data );
+  });
+});
+
+app.patch("/api/arco_registers", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la petición', req.body);
+  res.end( "Recibido!" )
+});
+
+app.post("/api/arco_registers", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+app.delete("/api/arco_registers", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+// ARCO type--------------------------
+
+app.get("/api/arco_type", (req, res) => {
+  fs.readFile( __dirname + "/" + "users.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(400).send("Error al leer el archivo JSON");
+    }
+    console.log( data );
+    res.end( data );
+  });
+});
+
+app.patch("/api/arco_type", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la petición', req.body);
+  res.end( "Recibido!" )
+});
+
+app.post("/api/arco_type", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+app.delete("/api/arco_type", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+// Addresses--------------------------
+
+app.get("/api/addresses", (req, res) => {
+  fs.readFile( __dirname + "/" + "users.json", "utf8", (err, data) => {
+    if (err) {
+      res.status(400).send("Error al leer el archivo JSON");
+    }
+    console.log( data );
+    res.end( data );
+  });
+});
+
+app.patch("/api/addresses", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la petición', req.body);
+  res.end( "Recibido!" )
+});
+
+app.post("/api/addresses", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
+  console.log('El cuerpo de la peticion:', req.body);
+  res.end( "Recibido!" );
+});
+
+app.delete("/api/addresses", (req, res) => {
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).send('Error: El cuerpo de la solicitud está vacío.');
+  }
   console.log('El cuerpo de la peticion:', req.body);
   res.end( "Recibido!" );
 });
@@ -41,6 +240,8 @@ app.post("/api/users", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+
 
 /*connection.connect((error) => {
   if (error) {
--- kill_site.sh
+++
@@ -1,4 +0,0 @@
-if tmux list-sessions >/dev/null 2>&1; then
-    tmux send-keys -t 0 C-c
-fi
-tmux kill-server
