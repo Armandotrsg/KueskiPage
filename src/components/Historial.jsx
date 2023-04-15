6a5b44d92b591cea038c23504aca3fa66f56a8df
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-14 20:11:13 -0600

    Cleaned the workspace, ModalAcceso ready

--- src/components/Historial.jsx
+++ src/components/Historial.jsx
@@ -1,16 +1,7 @@
-import { ModalAcceso } from "./ModalAcceso"
-import { useState } from "react"
-
 export const Historial = () => {
-    const [isOpen, setIsOpen] = useState(false)
     return (
         <div>
             <h1 className="text-2xl font-bold ml-4">Historial</h1>
-            <button
-                onClick={() => setIsOpen(true)}
-                className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 ml-4 rounded"
-            > Modal Acceso </button>
-            {isOpen && <ModalAcceso isOpen={isOpen} onClose={() => setIsOpen(false)} />}
         </div>
     )
 }
--- src/components/ModalAcceso.jsx
+++ src/components/ModalAcceso.jsx
@@ -7,65 +7,66 @@ import {
     ModalContainer,
 } from "./Modal";
 
-export const ModalAcceso = ({ isOpen, onClose, userData }) => {
-    userData = {
-        user_id: 1,
-        name: "John",
-        first_last_name: "Doe",
-        second_last_name: "Smith",
-        date_of_birth: "1990-01-01",
-        nationality: "American",
-        state_of_birth: "California",
-        economic_activity: "Employed",
-        curp: "1234567890",
-        gender: "Male",
-        phone_number: 1234567890,
-        email: "john.doe@example.com",
-        user_data: {
-            "contact_name": "John Doe",
+/* userData = {
+    user_id: 1,
+    name: "John",
+    first_last_name: "Doe",
+    second_last_name: "Smith",
+    date_of_birth: "1990-01-01",
+    nationality: "American",
+    state_of_birth: "California",
+    economic_activity: "Employed",
+    curp: "1234567890",
+    gender: "Male",
+    phone_number: 1234567890,
+    email: "john.doe@example.com",
+    user_data: {
+        "contact_name": "John Doe",
+    },
+    is_client: true,
+    is_blocked: false,
+    created_at: "2022-01-01",
+    updated_at: null,
+    deleted_at: null,
+    addresses: [
+        {
+            address_id: 1,
+            country: "USA",
+            state: "California",
+            city: "Los Angeles",
+            neighborhood: "",
+            zip_code: 90001,
+            street: "",
+            ext_number: 1,
+            int_number: null,
+            created_at: "",
+            updated_at: "",
+            deleted_at: "",
         },
-        is_client: true,
-        is_blocked: false,
-        created_at: "2022-01-01",
-        updated_at: null,
-        deleted_at: null,
-        addresses: [
-            {
-                address_id: 1,
-                country: "USA",
-                state: "California",
-                city: "Los Angeles",
-                neighborhood: "",
-                zip_code: 90001,
-                street: "",
-                ext_number: 1,
-                int_number: null,
-                created_at: "",
-                updated_at: "",
-                deleted_at: "",
-            },
-        ],
-        identification: [
-            {
-                identification_id: 1,
-                identification_type: "Passport",
-                identification_number: "1234567890",
-                created_at: "",
-                updated_at: "",
-                deleted_at: "",
-            },
-        ],
-        registros_arco: [
-            {
-                registro_arco_id: 1,
-                arco_type: "A",
-                message: "This is a message.",
-                created_at: "",
-                updated_at: "",
-                deleted_at: "",
-            },
-        ],
-    };
+    ],
+    identification: [
+        {
+            identification_id: 1,
+            identification_type: "Passport",
+            identification_number: "1234567890",
+            created_at: "",
+            updated_at: "",
+            deleted_at: "",
+        },
+    ],
+    registros_arco: [
+        {
+            registro_arco_id: 1,
+            arco_type: "A",
+            message: "This is a message.",
+            created_at: "",
+            updated_at: "",
+            deleted_at: "",
+        },
+    ],
+}; */
+
+export const ModalAcceso = ({ isOpen, onClose, userData }) => {
     const keys = Object.keys(userData);
     const addressesKeys = Object.keys(userData.addresses[0]);
     const identificationKeys = Object.keys(userData.identification[0]);
