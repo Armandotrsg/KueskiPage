403bf809a879ad3983ae170b991be9a9748f5eb8
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-16 15:29:37 -0600

    Falta unrender el component

--- src/components/Historial.jsx
+++ src/components/Historial.jsx
@@ -1,7 +1,13 @@
+import { Feedback } from "./Feedback"
+import { useState } from "react";
+
 export const Historial = () => {
+    const [feedback, setFeedback] = useState(false);
     return (
         <div>
             <h1 className="text-2xl font-bold ml-4">Historial</h1>
+            <button onClick={() => setFeedback(true)}>Mostrar feedback</button>
+            <Feedback feedback="Feedback" isVisible={feedback} setIsVisible={setFeedback} />
         </div>
     )
 }
