615ca27739148fd9e19005e4ab33ae5ae6496b78
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-17 12:32:38 -0600

    Navbar responsive for mobile

--- src/components/Historial.jsx
+++ src/components/Historial.jsx
@@ -1,20 +1,7 @@
-import { Feedback } from "./Feedback"
-import { useState } from "react";
-
 export const Historial = () => {
-    const [showFeedback, setShowFeedback] = useState(false);
-    function showMessage() {
-        setShowFeedback(true);
-        setTimeout(() => {
-            setShowFeedback(false);
-        }, 3500);
-    }
-
     return (
         <div>
             <h1 className="text-2xl font-bold ml-4">Historial</h1>
-            <button onClick={() => showMessage()}>Mostrar feedback</button>
-            {showFeedback && <Feedback feedback="Se actualizó la base de datos" />}
         </div>
     );
 };
