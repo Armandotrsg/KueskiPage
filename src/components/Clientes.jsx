e4cb966bb62ea0a810f00e1ea1e766cb13e740b9
Armando Terrazas Gómez <mandotrs@icloud.com>
2023-04-17 21:45:19 -0600

    nothing changed

--- src/components/Clientes.jsx
+++ src/components/Clientes.jsx
@@ -99,18 +99,18 @@ export const Clientes = () => {
     return (
         <div className="flex flex-col pl-5 pr-5 pb-5 h-screen w-screen overflow-scroll">
             {/*Título de la página*/}
-            <h1 className="text-2xl font-bold">Clientes</h1>
-            <div class=" overflow-x-auto shadow-md sm:rounded-lg mt-5"></div>
+            <h1 className="text-2xl font-bold mx-auto p-4">Clientes</h1>
+            <div className=" overflow-x-auto shadow-md sm:rounded-lg mt-5"></div>
 
             {/*Busqueda*/}
-            <div class="flex pb-4 bg-white dark:bg-white ">
-                <label for="table-search" class="sr-only">
+            <div className="flex pb-4 bg-white dark:bg-white ">
+                <label for="table-search" className="sr-only">
                     Search
                 </label>
-                <div class="flex flex-row-reverse relative mt-1 space-x-4">
-                    <div class="absolute inset-y-0 left-3.5 flex items-center pl-3 pointer-events-none">
+                <div className="flex flex-row-reverse relative mt-1 space-x-4">
+                    <div className="absolute inset-y-0 left-3.5 flex items-center pl-3 pointer-events-none">
                         <svg
-                            class="w-5 h-5 text-gray-500 dark:text-gray-400"
+                            className="w-5 h-5 text-gray-500 dark:text-gray-400"
                             aria-hidden="true"
                             fill="currentColor"
                             viewBox="0 0 20 20"
@@ -139,24 +139,24 @@ export const Clientes = () => {
                         value={searchText}
                         onChange={handleSearch}
                         id="table-search"
-                        class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-white-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Buscar"
                     />
                 </div>
             </div>
             {/*Filtro ARCO*/}
-            <div class="pb-1 bg-white dark:bg-white d-flex justify-content-start">
+            <div className="pb-1 bg-white dark:bg-white d-flex justify-content-start ml-4">
                 <button
                     id="dropdownAction"
                     onClick={handleDropdownToggle}
                     data-dropdown-toggle="dropdownAction"
-                    class="flex w-max items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-white dark:text-black dark:border-gray-200 dark:hover:bg-gray-300 dark:hover:border-gray-600 dark:focus:ring-gray-400"
+                    className="flex w-max items-center text-gray-500 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-white dark:text-black dark:border-gray-200 dark:hover:bg-gray-300 dark:hover:border-gray-600 dark:focus:ring-gray-400"
                     type="button"
                 >
-                    <span class="sr-only">Action button</span>
+                    <span className="sr-only">Action button</span>
                     Acción
                     <svg
-                        class="w-3 h-3 ml-2"
+                        className="w-3 h-3 ml-2"
                         aria-hidden="true"
                         fill="none"
                         stroke="currentColor"
@@ -175,10 +175,10 @@ export const Clientes = () => {
                 {dropdownMenuOpen && (
                     <div
                         id="dropdownActionButton"
-                        class="flex z-10 w-max text-black bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-white dark:text-black dark:border-gray-200 dark:hover:bg-gray-100 dark:hover:border-gray-100 dark:focus:ring-gray-200"
+                        className="flex z-10 w-max text-black bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-3 py-1.5 dark:bg-white dark:text-black dark:border-gray-200 dark:hover:bg-gray-100 dark:hover:border-gray-100 dark:focus:ring-gray-200"
                     >
                         <ul
-                            class="py-1 text-sm text-black dark:text-black"
+                            className="py-1 text-sm text-black dark:text-black"
                             aria-labelledby="dropdownActionButton"
                         >
                             {arcoRights.map((arcoRight, index) => {
@@ -186,7 +186,7 @@ export const Clientes = () => {
                                     <li key={index}>
                                         <a
                                             href="#"
-                                            class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-200 dark:hover:text-black font-medium rounded-lg text-sm"
+                                            className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-200 dark:hover:text-black font-medium rounded-lg text-sm"
                                         >{`${arcoRight} - ${arcoRight[0]}`}</a>
                                     </li>
                                 );
@@ -204,7 +204,7 @@ export const Clientes = () => {
                     progressPending={loading}
                     pagination
                     searchable
-                    class="z-0 w-full text-sm text-left text-black dark:text-black"
+                    className="z-0 w-full text-sm text-left text-black dark:text-black"
                 ></DataTable>
             </div>
         </div>
