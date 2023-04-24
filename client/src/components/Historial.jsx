d38e947474c67d09c24e9727d8d2fd761ffe97d8
HugoGoHe <A01640856@itesm.mx>
2023-04-24 14:57:32 -0600

    Ya funcionan los date pickers

--- client/src/components/Historial.jsx
+++ client/src/components/Historial.jsx
@@ -1,105 +1,187 @@
+import DataTable from "react-data-table-component";
+import { useState, useEffect } from "react";
+import data from "../shared/data.json";
 
 export const Historial = () => {
-const people = [
-    {
-      name: 'Leslie Alexander',
-      email: 'leslie.alexander@example.com',
-      role: 'Co-Founder / CEO',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Michael Foster',
-      email: 'michael.foster@example.com',
-      role: 'Co-Founder / CTO',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Dries Vincent',
-      email: 'dries.vincent@example.com',
-      role: 'Business Relations',
-      lastSeen: null,
-    },
-    {
-      name: 'Lindsay Walton',
-      email: 'lindsay.walton@example.com',
-      role: 'Front-end Developer',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Courtney Henry',
-      email: 'courtney.henry@example.com',
-      role: 'Designer',
-      lastSeen: '3h ago',
-      lastSeenDateTime: '2023-01-23T13:23Z',
-    },
-    {
-      name: 'Tom Cook',
-      email: 'tom.cook@example.com',
-      role: 'Director of Product',
-      lastSeen: null,
-    },
-  ]
-  
-  return (
-    <div className="flex flex-col pl-5 pr-5 pb-5 h-screen w-screen overflow-scroll">
-      <h1 className="text-2xl font-bold mx-auto p-4">Historial</h1>
-      <ul role="list" className="divide-y divide-gray-100">
-        {people.map((person) => (
-          <li key={person.email} className="flex flex-wrap justify-between gap-6 py-5">
-            <div className="flex flex-col w-full sm:w-auto justify-between">
-              <div className="min-w-0">
-                <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
-              </div>
-              {person.role && (
-                <p className="text-sm leading-6 text-gray-900">{person.role}</p>
-              )}
-              {person.lastSeen ? (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
-                </p>
-              ) : (
-                <div className="mt-1 flex items-center gap-x-1.5">
-                  <div className="flex-none rounded-full bg-emerald-500/20 p-1">
-                    <div className="h-1.5 w-1.5 rounded-full bg-emerald-500" />
-                  </div>
-                  <p className="text-xs leading-5 text-gray-500">Online</p>
-                </div>
-              )}
-            </div>
-            <div className="flex flex-col w-full sm:w-auto justify-between">
-              <div className="min-w-0">
-                <p className="text-sm font-semibold leading-6 text-gray-900">Campo 2</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">Texto de ejemplo</p>
-              </div>
-              <div className="min-w-0">
-                <p className="text-sm font-semibold leading-6 text-gray-900">Campo 3</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">Texto de ejemplo</p>
-              </div>
-            </div>
-            <div className="flex flex-col w-full sm:w-auto justify-between">
-              <div className="min-w-0">
-                <p className="text-sm font-semibold leading-6 text-gray-900">Campo 4</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">Texto de ejemplo</p>
-              </div>
-              <div className="min-w-0">
-                <p className="text-sm font-semibold leading-6 text-gray-900">Campo 5</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">Texto de ejemplo</p>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-  
-  
-  
+    const [tableData, setTableData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [searchText, setSearchText] = useState("");
+    const [searchBy, setSearchBy] = useState("name"); // Por defecto, buscar en la columna "name"
+    const [actionButton, setActionButton] = useState("Acceso");
 
-  
-  
+    const arcoRights = ["Acceso", "Rectificación", "Cancelación", "Oposición"];
+    //const filtroBusqueda = ["ID", "Nombre", "Apellido Paterno", "Apellido Materno", "CURP", "RFC"];
+
+    const test = [
+        {
+            id: 1,
+            arco: "Acceso",
+            userid: 4,
+            mensaje: "Quiero tener acceso a mis datos personales",
+            creacion: "01/03/2023",
+            fechacump: "20/03/2023",
+        },
+        {
+            id: 2,
+            arco: "Rectificación",
+            userid: 8,
+            mensaje: "Necesito corregir mi dirección en el registro",
+            creacion: "05/03/2023",
+            fechacump: "31/03/2023",
+        },
+        {
+            id: 3,
+            arco: "Cancelación",
+            userid: 2,
+            mensaje: "Quiero cancelar mi cuenta y borrar todos mis datos",
+            creacion: "10/03/2023",
+            fechacump: "30/03/2023",
+        },
+        {
+            id: 4,
+            arco: "Oposición",
+            userid: 11,
+            mensaje: "No quiero que mis datos sean compartidos con terceros",
+            creacion: "15/03/2023",
+            fechacump: "05/04/2023",
+        },
+        {
+            id: 5,
+            arco: "Acceso",
+            userid: 7,
+            mensaje: "Quiero saber qué datos personales tiene la empresa sobre mí",
+            creacion: "20/03/2023",
+            fechacump: "10/04/2023",
+        },
+        {
+            id: 6,
+            arco: "Rectificación",
+            userid: 5,
+            mensaje: "Mi nombre está mal escrito en el registro, necesito corregirlo",
+            creacion: "25/03/2023",
+            fechacump: "15/04/2023",
+        },
+    ]
+
+    const columns = [
+        {
+            name: "ID",
+            selector: row => row.id,
+            sortable: true,
+            center: true,
+            grow: 0.5,
+            width: "60px",
+            style: { fontWeight: "bold" },
+            format: (row) => <div>{row.id}</div>,
+        },
+        {
+            name: "ARCO",
+            selector: row => row.arco,
+            sortable: true,
+            center: true,
+            grow: 1,
+            width: "flex",
+            format: (row) => <div>{row.arco}</div>,
+        },
+        {
+            name: "User ID",
+            selector: row => row.userid,
+            sortable: true,
+            center: true,
+            grow: 1,
+            width: "flex",
+            format: (row) => <div>{row.userid}</div>,
+        },
+        {
+            name: "Mensaje",
+            selector: row => row.mensaje,
+            sortable: true,
+            center: true,
+            grow: 1,
+            width: "flex",
+            format: (row) => <div>{row.mensaje}</div>,
+        },
+        {
+            name: "Creacion",
+            selector: row => row.creacion,
+            sortable: true,
+            center: true,
+            grow: 1,
+            width: "flex",
+            format: (row) => <div>{row.creacion}</div>,
+        },
+        {
+            name: "Fecha de cumplimiento",
+            selector: row => row.fechacump,
+            sortable: true,
+            center: true,
+            grow: 1,
+            width: "flex",
+            format: (row) => <div>{row.fechacump}</div>,
+        },
+    ];
+
+    useEffect(() => {
+        setLoading(true);
+        setTableData(data);
+        setLoading(false);
+    }, []);
+
+    const handleSearch = (e) => {
+        setSearchText(e.target.value);
+    };
+
+
+    const handleSearchByChange = (e) => { // Función para manejar el cambio de campo de búsqueda
+        setSearchBy(e.target.value);
+        setSearchText(""); // Borrar texto de búsqueda al cambiar el campo de búsqueda
+    };
+
+    const filteredData = tableData.filter((item) => // Buscar en el campo seleccionado
+    item[searchBy].toLowerCase().includes(searchText.toLowerCase())
+    );
+
     
+    // const dateRangePickerEl = document.getElementById('dateRangePickerId');
+    //      new DateRangePicker(dateRangePickerEl, {}); 
+
+
+    return (
+        <div className="flex flex-col pl-5 pr-5 pb-5 h-screen w-screen overflow-scroll">
+            {/*Título de la página*/}
+            <h1 className="text-2xl font-bold mx-auto p-4">Historial</h1>
+            <div className=" overflow-x-auto shadow-md sm:rounded-lg mt-5"></div>
+
+
+            <div>
+                <div date-rangepicker class="flex items-center">
+                    <div class="relative">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                        </div>
+                        <input name="start" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5 " placeholder="Select date start"></input>
+                    </div>
+                    <span class="mx-4 text-gray-500">to</span>
+                    <div class="relative">
+                        <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clip-rule="evenodd"></path></svg>
+                        </div>
+                        <input name="end" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Select date end"></input>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                {/* Creación de la tabla*/}
+                <DataTable
+                    columns={columns}
+                    data={test}
+                    progressPending={loading}
+                    pagination
+                    searchable
+                    className="z-0 w-full text-sm text-left text-black dark:text-black"
+                ></DataTable>
+            </div>
+        </div>
+    );
 };
