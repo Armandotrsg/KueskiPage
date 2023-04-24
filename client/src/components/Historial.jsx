c513f12aea0683e48d0a9a83659bff4aaf901cff
HugoGoHe <A01640856@itesm.mx>
2023-04-23 22:24:22 -0600

    Changes on the layout of the component, now it has 6 alligned fields

--- client/src/components/Historial.jsx
+++ client/src/components/Historial.jsx
@@ -5,8 +5,6 @@ const people = [
       name: 'Leslie Alexander',
       email: 'leslie.alexander@example.com',
       role: 'Co-Founder / CEO',
-      imageUrl:
-        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       lastSeen: '3h ago',
       lastSeenDateTime: '2023-01-23T13:23Z',
     },
@@ -14,8 +12,6 @@ const people = [
       name: 'Michael Foster',
       email: 'michael.foster@example.com',
       role: 'Co-Founder / CTO',
-      imageUrl:
-        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       lastSeen: '3h ago',
       lastSeenDateTime: '2023-01-23T13:23Z',
     },
@@ -23,16 +19,12 @@ const people = [
       name: 'Dries Vincent',
       email: 'dries.vincent@example.com',
       role: 'Business Relations',
-      imageUrl:
-        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       lastSeen: null,
     },
     {
       name: 'Lindsay Walton',
       email: 'lindsay.walton@example.com',
       role: 'Front-end Developer',
-      imageUrl:
-        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       lastSeen: '3h ago',
       lastSeenDateTime: '2023-01-23T13:23Z',
     },
@@ -40,8 +32,6 @@ const people = [
       name: 'Courtney Henry',
       email: 'courtney.henry@example.com',
       role: 'Designer',
-      imageUrl:
-        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       lastSeen: '3h ago',
       lastSeenDateTime: '2023-01-23T13:23Z',
     },
@@ -49,29 +39,24 @@ const people = [
       name: 'Tom Cook',
       email: 'tom.cook@example.com',
       role: 'Director of Product',
-      imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
       lastSeen: null,
     },
   ]
   
-    return (
-        <div className="flex flex-col pl-5 pr-5 pb-5 h-screen w-screen overflow-scroll">
-        <h1 className="text-2xl font-bold mx-auto p-4">Historial</h1>
-
-
+  return (
+    <div className="flex flex-col pl-5 pr-5 pb-5 h-screen w-screen overflow-scroll">
+      <h1 className="text-2xl font-bold mx-auto p-4">Historial</h1>
       <ul role="list" className="divide-y divide-gray-100">
         {people.map((person) => (
-          <li key={person.email} className="flex justify-between gap-x-6 py-5">
-            <div className="flex gap-x-4">
-              <img className="h-12 w-12 flex-none rounded-full bg-gray-50" src={person.imageUrl} alt="" />
-              <div className="min-w-0 flex-auto">
+          <li key={person.email} className="flex flex-wrap justify-between gap-6 py-5">
+            <div className="flex flex-col w-full sm:w-auto justify-between">
+              <div className="min-w-0">
                 <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
                 <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
               </div>
-            </div>
-            <div className="hidden sm:flex sm:flex-col sm:items-end">
-              <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+              {person.role && (
+                <p className="text-sm leading-6 text-gray-900">{person.role}</p>
+              )}
               {person.lastSeen ? (
                 <p className="mt-1 text-xs leading-5 text-gray-500">
                   Last seen <time dateTime={person.lastSeenDateTime}>{person.lastSeen}</time>
@@ -85,11 +70,35 @@ const people = [
                 </div>
               )}
             </div>
+            <div className="flex flex-col w-full sm:w-auto justify-between">
+              <div className="min-w-0">
+                <p className="text-sm font-semibold leading-6 text-gray-900">Campo 2</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">Texto de ejemplo</p>
+              </div>
+              <div className="min-w-0">
+                <p className="text-sm font-semibold leading-6 text-gray-900">Campo 3</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">Texto de ejemplo</p>
+              </div>
+            </div>
+            <div className="flex flex-col w-full sm:w-auto justify-between">
+              <div className="min-w-0">
+                <p className="text-sm font-semibold leading-6 text-gray-900">Campo 4</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">Texto de ejemplo</p>
+              </div>
+              <div className="min-w-0">
+                <p className="text-sm font-semibold leading-6 text-gray-900">Campo 5</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">Texto de ejemplo</p>
+              </div>
+            </div>
           </li>
         ))}
       </ul>
     </div>
-    )
+  );
+  
+  
+  
+
   
   
     
