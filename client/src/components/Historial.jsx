df694fae66aa75de6f99b5e37db520308cc23838
Armando Terrazas Gómez <A01640924@tec.mx>
2023-04-24 19:43:39 -0600

    Los datos ya los obtiene de la API y borré archivos extra

--- client/src/components/Historial.jsx
+++ client/src/components/Historial.jsx
@@ -1,150 +1,95 @@
 import DataTable from "react-data-table-component";
 import { useState, useEffect } from "react";
-import data from "../shared/data.json";
+import { Loader } from "./Loader";
 
 export const Historial = () => {
     const [tableData, setTableData] = useState([]);
     const [loading, setLoading] = useState(false);
-    const [searchText, setSearchText] = useState("");
-    const [searchBy, setSearchBy] = useState("name"); // Por defecto, buscar en la columna "name"
-    const [actionButton, setActionButton] = useState("Acceso");
-
-    const arcoRights = ["Acceso", "Rectificación", "Cancelación", "Oposición"];
-    //const filtroBusqueda = ["ID", "Nombre", "Apellido Paterno", "Apellido Materno", "CURP", "RFC"];
-
-    const test = [
-        {
-            id: 1,
-            arco: "Acceso",
-            userid: 4,
-            mensaje: "Quiero tener acceso a mis datos personales",
-            creacion: "01/03/2023",
-            fechacump: "20/03/2023",
-        },
-        {
-            id: 2,
-            arco: "Rectificación",
-            userid: 8,
-            mensaje: "Necesito corregir mi dirección en el registro",
-            creacion: "05/03/2023",
-            fechacump: "31/03/2023",
-        },
-        {
-            id: 3,
-            arco: "Cancelación",
-            userid: 2,
-            mensaje: "Quiero cancelar mi cuenta y borrar todos mis datos",
-            creacion: "10/03/2023",
-            fechacump: "30/03/2023",
-        },
-        {
-            id: 4,
-            arco: "Oposición",
-            userid: 11,
-            mensaje: "No quiero que mis datos sean compartidos con terceros",
-            creacion: "15/03/2023",
-            fechacump: "05/04/2023",
-        },
-        {
-            id: 5,
-            arco: "Acceso",
-            userid: 7,
-            mensaje: "Quiero saber qué datos personales tiene la empresa sobre mí",
-            creacion: "20/03/2023",
-            fechacump: "10/04/2023",
-        },
-        {
-            id: 6,
-            arco: "Rectificación",
-            userid: 5,
-            mensaje: "Mi nombre está mal escrito en el registro, necesito corregirlo",
-            creacion: "25/03/2023",
-            fechacump: "15/04/2023",
-        },
-    ]
-
+    const [shouldRefetch, setShouldRefetch] = useState(false);
+    // const [searchText, setSearchText] = useState("");
+    // const [searchBy, setSearchBy] = useState("name"); // Por defecto, buscar en la columna "name"
+    
     const columns = [
         {
             name: "ID",
-            selector: row => row.id,
+            selector: row => row.registro_arco_id,
             sortable: true,
             center: true,
             grow: 0.5,
             width: "60px",
             style: { fontWeight: "bold" },
-            format: (row) => <div>{row.id}</div>,
-        },
-        {
-            name: "ARCO",
-            selector: row => row.arco,
-            sortable: true,
-            center: true,
-            grow: 1,
-            width: "flex",
-            format: (row) => <div>{row.arco}</div>,
+            format: (row) => <div>{row.registro_arco_id}</div>,
         },
         {
             name: "User ID",
-            selector: row => row.userid,
+            selector: row => row.user_id,
             sortable: true,
             center: true,
             grow: 1,
             width: "flex",
-            format: (row) => <div>{row.userid}</div>,
+            format: (row) => <div>{row.user_id}</div>,
+        },
+        {
+            name: "Derecho ARCO",
+            selector: row => row.arco_type,
+            sortable: true,
+            center: true,
+            grow: 1,
+            width: "flex",
+            format: (row) => <div>{row.arco_type === "A" ? "Acceso" : row.arco_type === "R" ? "Rectificación" : row.arco_type === "C" ? "Cancelación" : "Oposición"}</div>,
         },
         {
             name: "Mensaje",
-            selector: row => row.mensaje,
+            selector: row => row.message,
             sortable: true,
             center: true,
             grow: 1,
             width: "flex",
-            format: (row) => <div>{row.mensaje}</div>,
-        },
-        {
-            name: "Creacion",
-            selector: row => row.creacion,
-            sortable: true,
-            center: true,
-            grow: 1,
-            width: "flex",
-            format: (row) => <div>{row.creacion}</div>,
+            format: (row) => <div>{row.message}</div>,
         },
         {
             name: "Fecha de cumplimiento",
-            selector: row => row.fechacump,
+            selector: row => row.created_at,
             sortable: true,
             center: true,
             grow: 1,
             width: "flex",
-            format: (row) => <div>{row.fechacump}</div>,
+            format: (row) => <div>{row.created_at.split("T")[0]}</div>,
         },
     ];
 
-    useEffect(() => {
+   const fetchTableData = () => {
         setLoading(true);
-        setTableData(data);
-        setLoading(false);
-    }, []);
+        fetch("/api/arco_registers")
+            .then((res) => res.json())
+            .then((data) => {
+                setTableData(data);
+                console.log(data);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => {
+                setLoading(false);
+            });
+   }
 
-    const handleSearch = (e) => {
+    useEffect(() => {
+        fetchTableData();
+    }, [shouldRefetch]);
+
+    /* const handleSearch = (e) => {
         setSearchText(e.target.value);
-    };
+    }; */
 
-
-    const handleSearchByChange = (e) => { // Función para manejar el cambio de campo de búsqueda
+   /*  const handleSearchByChange = (e) => { // Función para manejar el cambio de campo de búsqueda
         setSearchBy(e.target.value);
         setSearchText(""); // Borrar texto de búsqueda al cambiar el campo de búsqueda
-    };
+    }; */
 
-    const filteredData = tableData.filter((item) => // Buscar en el campo seleccionado
-    item[searchBy].toLowerCase().includes(searchText.toLowerCase())
-    );
-
-    
-    // const dateRangePickerEl = document.getElementById('dateRangePickerId');
-    //      new DateRangePicker(dateRangePickerEl, {}); 
-
+    /* const filteredData = tableData.filter((item) => // Buscar en el campo seleccionado
+        item[searchBy].toLowerCase().includes(searchText.toLowerCase())
+    ); */
 
     return (
         <div className="flex flex-col pl-5 pr-5 pb-5 h-screen w-screen overflow-scroll">
@@ -152,8 +97,7 @@ export const Historial = () => {
             <h1 className="text-2xl font-bold mx-auto p-4">Historial</h1>
             <div className=" overflow-x-auto shadow-md sm:rounded-lg mt-5"></div>
 
-
-            <div>
+            <section>
                 <div class="flex items-center">
                     <div class="relative">
                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
@@ -169,19 +113,25 @@ export const Historial = () => {
                         <input name="end" type="date" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5" placeholder="Select date end"></input>
                     </div>
                 </div>
-            </div>
+            </section>
 
-            <div>
+            <main>
                 {/* Creación de la tabla*/}
-                <DataTable
-                    columns={columns}
-                    data={test}
-                    progressPending={loading}
-                    pagination
-                    searchable
-                    className="z-0 w-full text-sm text-left text-black dark:text-black"
-                ></DataTable>
-            </div>
+                {
+                    loading ? (
+                        <Loader />
+                    ) : (
+                        <DataTable
+                            columns={columns}
+                            data={tableData}
+                            progressPending={loading}
+                            pagination
+                            searchable
+                            className="z-0 w-full text-sm text-left text-black dark:text-black"
+                        ></DataTable>
+                    )
+                }
+            </main>
         </div>
     );
 };
