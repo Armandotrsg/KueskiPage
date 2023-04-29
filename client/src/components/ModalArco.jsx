75fca63cc247b979ccc0af7478b48b95239d8874
HugoGoHe <A01640856@itesm.mx>
2023-04-28 23:05:56 -0600

    Cambios menores

--- client/src/components/ModalArco.jsx
+++ client/src/components/ModalArco.jsx
@@ -12,7 +12,6 @@ export const ModalArco = ({
     isOpen,
     onClose,
     userData
-    //arcoRegister
     
     ,}) => {
 
@@ -25,9 +24,48 @@ export const ModalArco = ({
         );
     }
 
+    const keys = Object.keys(userData);
+
+    //Este bloque de codigo no se si este funcionando bien
+    let arco = "hola";
+    console.log('lkdksadfla')
+    if(userData.arco_type == "A")
+    {
+        arco = "Acceso"
+    }
+    else if(userData.arco_type == "R")
+    {
+        arco = "Registro"
+    }
+    else if(userData.arco_type == "C")
+    {
+        arco = "Correcion"
+    }
+    else if(userData.arco_type == "O")
+    {
+        arco = "Oposicion"
+    }
+
     return(
-        <Modal isOpen={isOpen} onClose={onClose}>
-            {userData.user_id}        
+        
+        <Modal isOpen={isOpen} onClose={onClose}
+        className={`w-[98%] sm:w-[75%] h-[75%]`}
+        >
+            <div >
+                {/* Titulo del modal */}
+                <ModalTitle>Informacion</ModalTitle>
+                {/* Contenido del modal */}
+
+                <ModalContainer title="Datos de solicitud">
+                    <div>
+                        UserID: {userData.user_id} <br />
+                        Derecho Arco: {arco} <br />
+                        Mensaje: {userData.message} <br />
+                        Fecha de cumplimiento: {userData.created_at.split("T")[0]} <br />
+                    </div>
+
+                </ModalContainer>
+            </div>
         </Modal>
     )
     
