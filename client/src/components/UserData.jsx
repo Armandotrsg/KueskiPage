67d791754f4304a5859562e298773b97d7b7e6d7
Armando Terrazas Gómez <A01640924@tec.mx>
2023-04-28 19:02:12 -0600

    Al corregir los datos se guarda en el registro el atributo traducido a español que cambió

--- client/src/components/UserData.jsx
+++ client/src/components/UserData.jsx
@@ -1,57 +1,19 @@
 import { useState } from "react";
 import { Checkbox } from "./Checkbox";
+import {
+    spanishKeysAddresses,
+    spanishKeysIdentifications,
+    spanishKeysUsers,
+} from "../shared/SpanishKeys";
 
 export const UserData = ({ atributo, valor, isEditable, id }) => {
     const [isChecked, setIsChecked] = useState(true);
     let [inputText, setInputText] = useState(valor);
 
-    const spanishKeysUsers = {
-        user_id: "ID de usuario",
-        name: "Nombre",
-        first_last_name: "Apellido paterno",
-        second_last_name: "Apellido materno",
-        date_of_birth: "Fecha de nacimiento",
-        nationality: "Nacionalidad",
-        state_of_birth: "Estado de nacimiento",
-        economic_activity: "Actividad económica",
-        curp: "CURP",
-        gender: "Género",
-        phone_number: "Número de teléfono",
-        email: "Email",
-        is_client: "Es Cliente",
-        is_blocked: "Está Bloqueado",
-        created_at: "Fecha de creación",
-        updated_at: "Fecha de actualización",
-        deleted_at: "Fecha de eliminación",
-        rfc: "RFC"
-    }
-    const spanishKeysAddresses = {
-        address_id: "ID de dirección",
-        country:  "País",
-        state: "Estado",
-        city: "Ciudad",
-        neighborhood: "Colonia",
-        zip_code: "Código postal",
-        street: "Calle",
-        ext_number: "Número exterior",
-        int_number: "Número interior",
-        created_at: "Fecha de creación",
-        updated_at: "Fecha de actualización",
-        deleted_at: "Fecha de eliminación",
-    }
-    const spanishKeysIdentifications = {
-        identification_id: "ID de identificación",
-        identification_type: "Tipo de identificación",
-        identification_number: "Número de identificación",
-        created_at: "Fecha de creación",
-        updated_at: "Fecha de actualización",
-        deleted_at: "Fecha de eliminación",
-    }
-
     const convertDateFormat = () => {
         valor = valor.split("T")[0];
         inputText = inputText.split("T")[0];
-    }
+    };
     if (atributo === "date_of_birth") {
         convertDateFormat();
     } else if (atributo.includes("_at")) {
@@ -80,8 +42,8 @@ export const UserData = ({ atributo, valor, isEditable, id }) => {
                 disabled={isChecked}
                 required
                 name={atributo}
-                attributeid = {id}
-                formerdata = {valor}
+                attributeid={id}
+                formerdata={valor}
             />
         );
     }
@@ -91,8 +53,22 @@ export const UserData = ({ atributo, valor, isEditable, id }) => {
                 input || isEditable ? "items-center" : ""
             }`}
         >
-            {isEditable && <Checkbox isChecked={isChecked} setIsChecked={setIsChecked} />}
-            <h5 className={`text-lg ${atributo.includes("_id") ? "font-bold" : "font-semibold"}`}>{`${spanishKeysUsers[atributo] || spanishKeysAddresses[atributo] || spanishKeysIdentifications[atributo] || atributo}: `} &nbsp;</h5>
+            {isEditable && (
+                <Checkbox isChecked={isChecked} setIsChecked={setIsChecked} />
+            )}
+            <h5
+                className={`text-lg ${
+                    atributo.includes("_id") ? "font-bold" : "font-semibold"
+                }`}
+            >
+                {`${
+                    spanishKeysUsers[atributo] ||
+                    spanishKeysAddresses[atributo] ||
+                    spanishKeysIdentifications[atributo] ||
+                    atributo
+                }: `}{" "}
+                &nbsp;
+            </h5>
             {isEditable ? (
                 input
             ) : (
---
+++ client/src/shared/SpanishKeys.js
@@ -0,0 +1,42 @@
+export const spanishKeysUsers = {
+    user_id: "ID de usuario",
+    name: "Nombre",
+    first_last_name: "Apellido paterno",
+    second_last_name: "Apellido materno",
+    date_of_birth: "Fecha de nacimiento",
+    nationality: "Nacionalidad",
+    state_of_birth: "Estado de nacimiento",
+    economic_activity: "Actividad económica",
+    curp: "CURP",
+    gender: "Género",
+    phone_number: "Número de teléfono",
+    email: "Email",
+    is_client: "Es Cliente",
+    is_blocked: "Está Bloqueado",
+    created_at: "Fecha de creación",
+    updated_at: "Fecha de actualización",
+    deleted_at: "Fecha de eliminación",
+    rfc: "RFC"
+}
+export const spanishKeysAddresses = {
+    address_id: "ID de dirección",
+    country:  "País",
+    state: "Estado",
+    city: "Ciudad",
+    neighborhood: "Colonia",
+    zip_code: "Código postal",
+    street: "Calle",
+    ext_number: "Número exterior",
+    int_number: "Número interior",
+    created_at: "Fecha de creación",
+    updated_at: "Fecha de actualización",
+    deleted_at: "Fecha de eliminación",
+}
+export const spanishKeysIdentifications = {
+    identification_id: "ID de identificación",
+    identification_type: "Tipo de identificación",
+    identification_number: "Número de identificación",
+    created_at: "Fecha de creación",
+    updated_at: "Fecha de actualización",
+    deleted_at: "Fecha de eliminación",
+}
